59fb7f7bade6f21ca020679e8f30599d720a88ee
meghansh36 <meghansh.work@gmail.com>
2018-06-14 15:55:27 +0530

    first commit

--- fe-client/src/app/app.module.ts
+++ fe-client/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FeRootModule } from '@L1Path/fe-root.module';
+FeRootModule
 @NgModule({
   declarations: [
     AppComponent
