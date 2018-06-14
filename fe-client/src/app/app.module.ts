a198f167ce6ca969087b4e6d9bacb5801a8191fa
sahilm142 <sahil.mansoori.143@gmail.com>
2018-06-14 16:32:00 +0530

    Modified legislation folder

--- fe-client/src/app/app.module.ts
+++ fe-client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeRootModule } from '@L1Path/fe-root.module';
-FeRootModule
+
 @NgModule({
   declarations: [
     AppComponent
